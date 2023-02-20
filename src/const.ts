0fcd040d8119680318a2cd63f061f012c189fa51
Me1e <mele0404@naver.com>
2023-02-21 01:15:22 +0900

    feat: change server address

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
-export const SERVER_ADDRESS = 'http://172.30.1.60:3000';
-// const SERVER_ADDRESS = 'https://cloud4.mele.kr';
+// export const SERVER_ADDRESS = 'http://172.30.1.60:3000';
+export const SERVER_ADDRESS = 'https://cloud4.mele.kr';
 
 export const S3_ADDRESS_CLOTHES =
   'https://melebucket.s3.ap-northeast-2.amazonaws.com/assets';
